ec0ecca03d4607b56ff6a84c99559965287fff3e
tobe <tobeg3oogle@gmail.com>
2015-12-20 14:56:31 +0800

    Use docker-py to start container

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 #!/usr/bin/env python
 
+import docker
+
+
 def main():
     print("Start lambda-docker service")
 
+    client = docker.Client(base_url="unix:///var/run/docker.sock",
+                           version="auto")
 
+    runtime_image = "python"
+    command = "ls /lambda"
 
+    function_path = ""
+    container_path = ""
 
+    container = client.create_container(image=runtime_image,
+                                        command=command,
+                                        volumes=["/lambda"],
+                                        host_config=client.create_host_config(
+                                            binds=[
+                                                "/etc/vim/:/lambda:ro",
+                                            ])
+                                        )
+
+    container_id = container.get("Id")
+
+    client.start(container_id)
+    print(client.logs(container_id))
 
     print("End lambda-docker service")
-
 
 
 if __name__ == "__main__":
