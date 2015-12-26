90ec6344e8d64edd417f76526fd270aa7e5a3d03
tobe <tobeg3oogle@gmail.com>
2015-12-26 17:38:07 +0800

    Implement ubuntu container

---
+++ runtime/ubuntu_container.py
@@ -0,0 +1,12 @@
+
+import basic_container
+
+class UbuntuContainer(basic_container.BasicContainer):
+
+    def __init__(self):
+	super(self.__class__, self).__init__()
+        
+	self.image = "ubuntu"
+	self.command = "bash /tmp/main"
+	self.file_extension = ""
+
